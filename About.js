607963c75a159f5df6036ab92f97ad359f6fdfef
Fernando Rojas <Fernando.rojas0422@gmail.com>
2024-10-30 16:49:30 -0700

    Updates

---
+++ About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>
+        I am Fernando Rojas, a developer and UX/UI designer passionate about creating seamless digital experiences.
+      </p>
+    </section>
+  );
+}
+
+export default About;
